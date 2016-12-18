b5ad8215e638bc4531ff4cd7109ffbc0e8a8eead
OliverClarity <trippe@oliverclarity.com>
2016-12-18 16:54:48 +0000

    Seeded User Database via Faker

--- app/controllers/attorneys_controller.rb
+++ app/controllers/attorneys_controller.rb
@@ -1,2 +1,43 @@
 class AttorneysController < ApplicationController
+    
+def index
+    @attorneys = Attorneys.all
+end
+
+def new
+    @attorney = Attorney.new
+end
+
+def create
+    @attorney = Attorney.new(attorney_params)
+    if @attorney.save
+        flash[:success] = "New attorney added"
+        render 'index'
+    else
+        render 'new'
+    end
+end
+
+def update
+    @attorney = Attorney.find(params[:id])
+    if @attorney.update(post_params)
+        redirect_to @attorney
+    else
+        render :edit
+    end
+end
+
+def show
+    @attorney = Attorney.find(params[:id])
+end
+
+def destroy
+end
+
+private
+
+def attorney_params
+    params.require(:attorney).permit(:name, :address, :email, :phone, :license, :states, :practice)
+end
+
 end
