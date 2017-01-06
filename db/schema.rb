5e4ed46a712d2905adbb9c90ff0b367103bb500e
Cathy Cullen <cathy@softwareoptions.com>
2017-01-06 12:10:55 -0600

    added navbar

--- db/schema.rb
+++ db/schema.rb
@@ -104,6 +104,12 @@ ActiveRecord::Schema.define(version: 20170106014722) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "todos", force: :cascade do |t|
     t.string   "item"
     t.string   "date"
