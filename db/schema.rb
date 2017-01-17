a3fbe6922360831a24cce47971088971cab7da0c
Steven Fried <trippe1993@gmail.com>
2017-01-17 19:37:02 +0000

    Changed subscribe button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116170133) do
+ActiveRecord::Schema.define(version: 20170116164809) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "user_id"
@@ -119,12 +119,6 @@ ActiveRecord::Schema.define(version: 20170116170133) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_minutes_on_user_id"
-  end
-
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "todos", force: :cascade do |t|
